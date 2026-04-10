e296953c91340f3ddeb7fdc5d4a68956453631ff
kioscoplazasantabarbara-sketch <kioscoplazasantabarbara@gmail.com>
2026-04-10 17:35:34 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ═══════════════════════════════════════════════════════════════════
-   Dashwey Service Worker v1.3.734-dev
-   Cache: dashwey-v1-3-734-dev
+   Dashwey Service Worker v1.3.735-dev
+   Cache: dashwey-v1-3-735-dev
 
    ESTRATEGIA DE CACHE v1.0.1:
    - HTML principal: SIEMPRE network-only (NUNCA se cachea)
