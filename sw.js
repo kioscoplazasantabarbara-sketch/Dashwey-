de81d9982f4869fc9d91e5e67454d2b444df3466
kioscoplazasantabarbara-sketch <kioscoplazasantabarbara@gmail.com>
2026-04-16 16:35:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    - skipWaiting: inmediato siempre (manual y automático)
    ═══════════════════════════════════════════════════════════════════ */
 
-const CACHE_NAME  = 'dashwey-v1-3-922-dev';
+const CACHE_NAME  = 'dashwey-v1-3-923-dev';
 const HTML_URL    = 'index.html';
 
 /* Solo pre-cachear assets estáticos mínimos — NUNCA el HTML */
