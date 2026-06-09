5e7d839b7bf911494a3549a662f15a62b0e93505
kioscoplazasantabarbara-sketch <kioscoplazasantabarbara@gmail.com>
2026-06-09 12:46:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
    - skipWaiting: inmediato siempre (manual y automático).
    ═══════════════════════════════════════════════════════════════════ */
 
-const CACHE_NAME  = 'dashwey-v1-3-1979';
+const CACHE_NAME  = 'dashwey-v1-3-1980';
 const HTML_URL    = 'index.html';
 
 /* v1.3.1436 BUG-OFFLINE FIX — Lote D
